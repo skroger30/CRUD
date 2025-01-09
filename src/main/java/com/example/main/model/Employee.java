df459ccb2c291c12c6dea4c79e8c49b07e636f01
Sk <jksourabh30@gmail.com>
2025-01-09 15:12:35 +0530

    Adding Lombok Dependency

--- src/main/java/com/example/main/model/Employee.java
+++ src/main/java/com/example/main/model/Employee.java
@@ -5,7 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 public class Employee {
 
 	@Id
@@ -14,22 +19,5 @@ public class Employee {
 	private String empName;
 //	To implement soft-delete
 	private boolean isActive = true;
-	public int getEmpId() {
-		return empId;
-	}
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	public boolean isActive() {
-		return isActive;
-	}
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
+
 }
