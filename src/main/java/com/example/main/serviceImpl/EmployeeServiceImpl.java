6f33f3065d6277a499b69dd2e173474111620aee
Sk <jksourabh30@gmail.com>
2025-01-12 19:29:25 +0530

    Cache implementation Using External Cache Provider

--- src/main/java/com/example/main/serviceImpl/EmployeeServiceImpl.java
+++ src/main/java/com/example/main/serviceImpl/EmployeeServiceImpl.java
@@ -2,31 +2,46 @@ package com.example.main.serviceImpl;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.main.model.Employee;
 import com.example.main.repository.EmployeeRepository;
 import com.example.main.services.EmployeeServices;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeServices {
 
 	@Autowired
-	EmployeeRepository empRepo;
+	private EmployeeRepository empRepo;
 	
 	@Override
-	public void registerEmployee(List<Employee> empList) {
-		// TODO Auto-generated method stub
-		for(Employee emp : empList) {
-			empRepo.save(emp);
-		}		
+	public List<Employee> registerEmployee(List<Employee> empList) {
+		// TODO Auto-generated method stub		
+		 empRepo.saveAll(empList);
+		List<Employee>  list  = getAllCache.getIfPresent("Employees");
+		list.addAll(empList);
+		getAllCache.put("Employees", list);
+		return empList;
 	}
-
+ 
 	@Override
 	public List<Employee> getEmpData() {
 		// TODO Auto-generated method stub
-		return empRepo.findAll();
+        if(getAllCache.getIfPresent("Employees") !=  null){
+            List<Employee>  list  = getAllCache.getIfPresent("Employees");
+            System.out.println("we are retrieving the employee from the cache");
+            return list;
+        }
+        
+		List<Employee>allEmpls =empRepo.findAll();
+		getAllCache.put("Employees", allEmpls);
+		System.out.println("List of employees fetched from DB and inserted in cache.");
+		return allEmpls;
 	}
 
 	@Override
@@ -34,6 +49,15 @@ public class EmployeeServiceImpl implements EmployeeServices {
 		// TODO Auto-generated method stub
 		empRepo.deleteById(empId);
 	}
+
+//	Caching Implementation - Creating Cache Object
+	private Cache<String, List<Employee>> getAllCache;
 	
+	@PostConstruct
+	private void initCache() {
+		getAllCache = Caffeine.newBuilder()
+                .maximumSize(100)
+                .build();    
+	}
 	
 }
