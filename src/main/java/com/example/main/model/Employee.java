cff417e324eb0e34d1561e5a2d7a31dca4fb2252
Sk <jksourabh30@gmail.com>
2025-01-16 12:38:14 +0530

    Adding API Validation Functionality

--- src/main/java/com/example/main/model/Employee.java
+++ src/main/java/com/example/main/model/Employee.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,7 +23,10 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int empId;
+	
+	@NotEmpty(message = "Employee name should not be blank.") // Employee Name should not be null or empty.
 	private String empName;
+	
 //	To implement soft-delete
 	private boolean isActive = true;
 	private long mobileNo;
